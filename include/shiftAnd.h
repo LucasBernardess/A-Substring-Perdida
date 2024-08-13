f30ae947baecbd92a7f1db9a3344d98bb88f3498
Lucas Eduardo <117780585+LucasBernardess@users.noreply.github.com>
2024-08-13 18:00:34 -0300

    Retirando implementação do shiftAnd

--- include/shiftAnd.h
+++
@@ -1,9 +0,0 @@
-#ifndef SHIFTAND_H
-#define SHIFTAND_H
-
-#include <stdint.h>
-#include <stdio.h>
-
-void shift_and_with_indices(const char *text, const char *pattern, int num_intervals, int intervals[][2], FILE *outputFile);
-
-#endif
